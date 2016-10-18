d25484763eb7ed52478807a4edcc41e9bc10ebaf
Tim Hyde <livingos@gmail.com>
2016-10-18 20:48:43 +0100

    initial commit

---
+++ app/Http/Controllers/OAuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class OAuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+    public function oauth(){
+
+      $query = http_build_query([
+         'client_id'     => 6,
+         'redirect_uri'  => 'http://lumen.app/callback',
+         'response_type' => 'code',
+         'scope'         => 'search',
+       ]);
+
+      return redirect('http://laravel53.app/oauth/authorize?' . $query);
+
+    }
+
+    public function callback(Request $request){
+
+        $http = new Client;
+        $response = $http->post('http://laravel53.app/oauth/token', [
+            'form_params' => [
+                'grant_type'    => 'authorization_code',
+                'client_id'     => '6',
+                'client_secret' => '6HZTAgyG0BrzPHCZF8Ib27hKpfjCdq0feN3NZmgq',
+                'redirect_uri'  => 'http://lumen.app/callback',
+                'code'          => $request->code
+            ],
+        ]);
+
+        $tokens = json_decode((string)$response->getBody(), true);
+
+        //put in cache for now for trying out api routes below
+        Cache::put('access_token', $tokens['access_token'], 60);
+        Cache::put('refresh_token', $tokens['access_token'], 60);
+
+        return $tokens;
+
+    }
+
+    public function user()
+    {
+
+        // just for now we are saving the token in cache
+        $token = Cache::get('access_token');
+        $http = new Client;
+        $response = $http->get('http://laravel53.app/api/user', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Accepts'       => 'application/json',
+            ]
+        ]);
+
+
+        return $response->getBody();
+    }
+}
